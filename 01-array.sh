fcb7d4eafddcb25f617226e7fd8013e0d253dc8c
Devi Karnati <yireh.bramara@gmail.com>
2023-12-19 20:17:04 +0800

    this is array

---
+++ 01-array.sh
@@ -0,0 +1,5 @@
+person=("venu","devi","paul","Jeremy")
+echo "first person:  ${person[0]}"
+echo "second person: ${person[1]}"
+echo "third person:  ${person[2]}"
+echo "fourth person: ${person[3]}"
